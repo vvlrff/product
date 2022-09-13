f957895d2fee3205c2b3360e3147550b106dfff3
Artem <artemmorozov1308@gmail.com>
2022-09-13 15:22:55 +0300

    connected react react-dom added ability to import scss

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { render } from "react-dom";
+import { Header } from "./Components/Header/Header";
+
+render(
+    <div>
+        <Header />
+    </div>,
+    document.getElementById("root")
+)
