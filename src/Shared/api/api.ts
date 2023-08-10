37fc60c678175c9ba499fde99d0ae79f1d11eea4
olegmarkelov <vvlrff@mail.ru>
2023-08-10 17:49:38 +0500

    changed uri API

--- src/Shared/api/api.ts
+++ src/Shared/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { UserAccessTokenKey } from 'Shared/consts/AppConsts'
 
 export const API = axios.create({
-  baseURL: 'https://localhost:7084/api',
+  baseURL: 'https://forum-e1q8.onrender.com/api',
   headers: {
     Authorization: localStorage.getItem(UserAccessTokenKey)
   }
